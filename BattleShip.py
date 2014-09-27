b23bb6ac9697117eaf80cc4b5ce7eae5d4e6a3ce
Calthraun <jake@wilsonsword.com>
2014-09-27 15:02:15 +0800

    Added window title and improved dialogue
    Added a title for the console window (Windows only)
    Improved the dialogue.
    Changed coordinates so the player uses 1-5 instead of 0-4.

--- BattleShip.py
+++ BattleShip.py
@@ -1,6 +1,7 @@
 """ Based on lessons from codecademy.com """
 """ Imports """
 from random import randint #import randomizer for hiding ships
+from os import system #import window naming function
 
 """ Initialize main variables """
 board = []
@@ -18,57 +19,74 @@ def random_col(board):
     return randint(0, len(board[0]) - 1)
 
 """ Begin main program """
+#title window
+system("title BATTLESHIPS")
+
+#display introduction
+print "~BATTLESHIPS~"
+print "Destroy the enemy spy's submarine before he steals your secrets!"
+print "You have four missiles."
+print "Enter the coordinates you would like to fire at."
+print
+
 #generate 5x5 board
 for x in range(5):
     board.append(["O"] * 5)
 
-#display introduction and game board
-print "Let's play Battleship!"
+#display game board
 print_board(board)
+print
 
 #generate random ship coordinates
 ship_row = random_row(board)
 ship_col = random_col(board)
 
 #display coordinates for debugging
-#print ship_row
-#print ship_col
+print ship_row + 1
+print ship_col + 1
 
 #begin counting turns
 for turn in range(4):
     #collect guess input
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+    print "Enter two coordinates between 1 and 5."
+    guess_row = int(raw_input("Guess Row:")) - 1
+    guess_col = int(raw_input("Guess Column:")) - 1
+    print
     
     #check for hits
     if guess_row == ship_row and guess_col == ship_col:
         board[guess_row][guess_col] = "H"
         print_board(board)
-        print "Congratulations! You sunk my battleship!"
+        print "Congratulations! Your secrets are safe!"
         break
     
     #check for guesses out of range 
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
+            print "You missed the ocean! Aim more carefully next time."
+            print
         
         #check for conflicting guesses
         elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+            print "You fired there already! He's still not there..."
+            print
             
         #check for miss
         else:
-            print "You missed my battleship!"
+            print "Sploosh! You hit... some fish. Try again."
+            print
             board[guess_row][guess_col] = "X"
             
-        #display and increment turn number
+        #display turn number
         print "Turn", turn + 1
         
         #display updated board
         print_board(board)
+        print
         
         #check for game over
         if turn == 3:
+            print "The enemy spy has escaped with all your secrets..."
             print "Game Over"
 
 #give user a chance to read final output
